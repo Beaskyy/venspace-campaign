68e709ff0d0450c5793800e97f21376cdc9cbe57
Beaskyy <afeez.akintobi@yahoo.com>
2025-04-10 07:48:35 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
   // function onSubmit(values: z.infer<typeof formSchema>) {
   //   // Do something with the form values.
   //   // ✅ This will be type-safe and validated.
-  //   console.log(values);
   //   setShowSuccess(true);
   //   setLoading(true);
 
